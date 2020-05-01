1c494a670f060a54cc06d95c53b16cbbe0adf790
davidbhrs <d.baehrens@web.de>
2020-05-01 18:26:04 +0200

    Creation of basic razor page

--- ASPdotNet_StringVerarbeitung/ASPdotNet_StringVerarbeitung/Pages/Index.cshtml.cs
+++ ASPdotNet_StringVerarbeitung/ASPdotNet_StringVerarbeitung/Pages/Index.cshtml.cs
@@ -17,9 +17,51 @@ namespace ASPdotNet_StringVerarbeitung.Pages
             _logger = logger;
         }
 
+
+
+        /* Variablen */
+        [BindProperty]
+        public string inputString { get; set; }
+        
+        string newString = "";
+        char[] vokale = { 'a' , 'e', 'i', 'o', 'u'};
+        char newChar = ' ';
+
+
+
+        /* Methoden */
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            string[] words = inputString.Split(" ");
+            int wordNum = words.Length;
+            if (wordNum >= 10)
+            {
+                foreach (var character in inputString)
+                {
+                    if (vokale.Contains(character))
+                    {
+                        newChar = 'i';
+                        newString += newChar;
+                    }
+                    else
+                    {
+                        newString += character;
+                    }
+                }
+            }
+            else
+            {
+                newString = "Sie müssen mindestens 10 Wörter eingeben," +
+                    " um die App verwenden zu können. In Ihrer aktuellen " +
+                    $"Zeichenkette befinden sich allerdings nur { wordNum } " +
+                    "Wörter.";
+            }
+            ViewData["result"] = newString;
         }
     }
 }
