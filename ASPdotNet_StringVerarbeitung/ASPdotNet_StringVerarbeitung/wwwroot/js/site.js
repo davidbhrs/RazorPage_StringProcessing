57df5caa058178e353fb81506e1c67f98d2b525e
davidbhrs <d.baehrens@web.de>
2020-05-26 13:38:09 +0200

    cleaner Code

--- ASPdotNet_StringVerarbeitung/ASPdotNet_StringVerarbeitung/wwwroot/js/site.js
+++ ASPdotNet_StringVerarbeitung/ASPdotNet_StringVerarbeitung/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function resettingButton() {
+    document.getElementById("inputString").value = "";
+}
---
+++ ASPdotNet_StringVerarbeitung/ASPdotNet_StringVerarbeitung/wwwroot/js/statistic.js
@@ -0,0 +1,51 @@
+﻿a = Number(document.getElementById("aNum").value);
+e = Number(document.getElementById("eNum").value);
+o = Number(document.getElementById("oNum").value);
+u = Number(document.getElementById("uNum").value);
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Vokal', 'Änderungen'],
+        ['a', a],
+        ['e', e],
+        ['o', o],
+        ['u', u]
+    ]);
+
+    var options = {
+        title: 'Ersetzte Vokale',
+        backgroundColor: { fill: 'transparent' },
+        titleTextStyle: {
+            color: 'white'
+        },
+        hAxis: {
+            textStyle: {
+                color: 'white'
+            },
+            titleTextStyle: {
+                color: 'white'
+            }
+        },
+        vAxis: {
+            textStyle: {
+                color: 'white'
+            },
+            titleTextStyle: {
+                color: 'white'
+            }
+        },
+        legend: {
+            textStyle: {
+                color: 'white'
+            }
+        }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
